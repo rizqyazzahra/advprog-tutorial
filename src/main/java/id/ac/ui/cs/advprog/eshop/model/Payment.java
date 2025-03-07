56f11487b00032c5c8d1e5c95ac84985c3287059
rizqyazzahra <rizqyazzahraa@gmail.com>
2025-03-07 19:12:36 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,12 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
 import java.util.Map;
 
+@Builder
 @Getter
-@Setter
 public class Payment {
     String id;
     String method;
@@ -14,10 +16,35 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.paymentData = paymentData;
+        this.status = "PENDING";
 
+        String[] methodList = {"VOUCHER", "TRANSFER BANK"};
+        if (Arrays.stream(methodList).noneMatch(item -> (item.equals(method)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.method = method;
+        }
     }
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this(id, method, paymentData);
 
+        String[] statusList = {"PENDING", "SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"PENDING", "SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
