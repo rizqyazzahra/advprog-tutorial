c6abb5e7406fe4b3730de708323508228e0be150
rizqyazzahra <rizqyazzahraa@gmail.com>
2025-03-07 21:22:26 +0700

    [GREEN] Implement PaymentByVoucher Class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import java.util.Map;
 
 public class PaymentByVoucher extends Payment {
@@ -11,7 +12,23 @@ public class PaymentByVoucher extends Payment {
         super(id, method, status, paymentData);
     }
 
+    @Override
     public void setPaymentData(Map<String, String> paymentData) {
-
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            int numericalChar = 0;
+            for (char c : paymentData.get("voucherCode").toCharArray()) {
+                if (Character.isDigit(c)) {
+                    numericalChar++;
+                }
+            }
+            this.paymentData = paymentData;
+            if (paymentData.get("voucherCode").length() == 16 && paymentData.get("voucherCode").startsWith("ESHOP") && numericalChar == 8) {
+                this.status = PaymentStatus.SUCCESS.getValue();
+            } else {
+                this.status = PaymentStatus.REJECTED.getValue();
+            }
+        }
     }
 }
