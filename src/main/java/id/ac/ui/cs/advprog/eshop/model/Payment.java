ae232b7add6bd152b6665988e1b7cd87d1a1b89e
rizqyazzahra <rizqyazzahraa@gmail.com>
2025-03-07 21:20:53 +0700

    Add setter for paymentData

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -4,6 +4,7 @@ import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
@@ -13,6 +14,7 @@ public class Payment {
     String id;
     String method;
     String status;
+    @Setter
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
